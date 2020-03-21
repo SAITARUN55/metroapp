034e062b7c49e6eb438c6a64db3344ca34ac6ceb
iamriya <riaapatel1711@gmail.com>
2020-03-22 00:27:24 +0530

    Added icons for transport modes

--- lib/pages/transport_modes_page.dart
+++ lib/pages/transport_modes_page.dart
@@ -10,69 +10,66 @@ class TransportModesPage extends StatefulWidget {
 }
 
 class _TransportModesPageState extends State<TransportModesPage> {
-
-  List<TransportMode> list=[
-    TransportMode(title:"Metro",iconData:Icons.title,page:SearchRoutesPage()),
-    TransportMode(title:"Bus",iconData:Icons.access_alarm,page:SearchRoutesPage()),
-    TransportMode(title:"Train",iconData:Icons.print,page:SearchRoutesPage())
+  List<TransportMode> list = [
+    TransportMode(title: "Metro", icon: "assets/train.png", page: SearchRoutesPage()),
+    TransportMode(title: "Bus", icon: "assets/bus.png", page: SearchRoutesPage()),
+    TransportMode(title: "Car", icon: "assets/car.png", page: SearchRoutesPage()),
+    TransportMode(title: "Ship", icon: "assets/ship.png", page: SearchRoutesPage()),
+    TransportMode(title: "Plane", icon: "assets/plane.png", page: SearchRoutesPage()),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
-
         title: Text('Transport Modes'),
       ),
       body: Container(
-        child: GridView.builder(itemCount: list.length,gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), itemBuilder: (BuildContext context,int index){
-
-          return GestureDetector(
-
-            child: Card(
-
-              elevation: 5.0,
-
-              child: Container(
-
-                alignment: Alignment.center,
-
-                child:Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-
-                    Icon(list[index].iconData,size: 40,),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                        child:Text(list[index].title))
-
-
-                  ],
-                )
-
-
+        child: GridView.builder(
+          itemCount: list.length,
+          gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              child: Card(
+                elevation: 5.0,
+                child: Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: Image.asset(list[index].icon),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(list[index].title),
+                      )
+                    ],
+                  ),
+                ),
               ),
-            ),
-
-              onTap: (){
-
-                Navigator.push(context, MaterialPageRoute(builder: (_)=> TransportModeDetailsPage()));
-
-              }
-          );
-        })
-      )
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => TransportModeDetailsPage()));
+              },
+            );
+          },
+        ),
+      ),
     );
   }
 }
 
-
-class TransportMode{
-
+class TransportMode {
   String title;
-  IconData iconData;
+  String icon;
   Widget page;
 
-  TransportMode({@required this.title,@required this.iconData,@required this.page});
+  TransportMode(
+      {@required this.title, @required this.icon, @required this.page});
 }
