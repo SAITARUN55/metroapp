2e95913a316bd71590d076355bd2dd6eb383f59b
Aakanksha Shivani <shivaniaakanksha@gmail.com>
2020-03-24 07:47:10 +0530

    Shifted to AndroidX and Firebase Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:metroapp/fragments/about_frag.dart';
 import 'package:metroapp/fragments/fare_frag.dart';
@@ -11,22 +13,27 @@ import 'package:metroapp/fragments/settings_frag.dart';
 import 'package:metroapp/pages/cities_page.dart';
 import 'package:metroapp/fragments/share_frag.dart';
 
+FirebaseAnalytics analytics = FirebaseAnalytics();
 void main() {
-  runApp(new MaterialApp(
-    title: "Metro App",
-    routes: {
-      '/': (context) => CitiesPage(),
-      '/about': (context) => AboutFragment(),
-      '/routes': (context) => RoutesFragment(),
-      '/fare': (context) => FareFragment(),
-      '/metroMap': (context) => MetroMapFragment(),
-      '/settings': (context) => SettingsFragment(),
-      '/sendFeedback': (context) => SendFeedbackFragment(),
-      '/share': (context) => ShareFragment(),
-      '/rateTheApp': (context) => RateAppFragment(),
-      '/privacyPolicy': (context) => PrivacyPolicyFragment(),
-      '/Logout': (context) => LogoutFragment(),
-    },
-  ));
+  runApp(
+    new MaterialApp(
+      title: "Metro App",
+      routes: {
+        '/': (context) => CitiesPage(),
+        '/about': (context) => AboutFragment(),
+        '/routes': (context) => RoutesFragment(),
+        '/fare': (context) => FareFragment(),
+        '/metroMap': (context) => MetroMapFragment(),
+        '/settings': (context) => SettingsFragment(),
+        '/sendFeedback': (context) => SendFeedbackFragment(),
+        '/share': (context) => ShareFragment(),
+        '/rateTheApp': (context) => RateAppFragment(),
+        '/privacyPolicy': (context) => PrivacyPolicyFragment(),
+        '/Logout': (context) => LogoutFragment(),
+      },
+      navigatorObservers: [
+        FirebaseAnalyticsObserver(analytics: analytics),
+      ],
+    ),
+  );
 }
-
