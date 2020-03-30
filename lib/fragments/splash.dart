9a3287d24348c8c62d0dd7a9ef52262262c8b8a6
shilpi1958 <shilpi1958@gmail.com>
2020-03-30 15:43:24 +0530

    Update splash.dart

--- lib/fragments/splash.dart
+++ lib/fragments/splash.dart
@@ -10,7 +10,7 @@ class _MyAppState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
     return new SplashScreen(
-        seconds: 14,
+        seconds: 5,
         navigateAfterSeconds: '/intro',
         title: new Text('Welcome ',
           style: new TextStyle(
